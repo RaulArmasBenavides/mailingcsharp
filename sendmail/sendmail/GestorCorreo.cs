f2c37fdd479405cda6ad1945b4b262d27b702acc
Raúl Armas <raularmasbx@gmail.com>
2022-01-18 18:02:53 -0500

    creating..

---
+++ sendmail/sendmail/GestorCorreo.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Configuration;
+using MimeKit;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace sendmail
+{
+    public class GestorCorreo
+    {
+
+        private SmtpClient cliente;
+        private static IConfiguration Configuration { get; set; }
+        private MailMessage email;
+        public GestorCorreo()
+        {
+        }
+
+        //using MailKit
+        public void EnviarCorreo(string destinatario, string asunto, string mensaje, bool esHtlm = true)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("SISTEMA CERSEU", "imanol123valera@gmail.co"));
+            // message.To.Add(new MailboxAddress("Ing. Carlos Mamani", "12170115@unmsm.edu.pe"));
+            message.To.Add(new MailboxAddress("Ing. Carlos Mamani", "raularmasbx@gmail.com"));
+            message.Subject = "Notificación de registro de evento";
+
+            message.Body = new TextPart("plain")
+            {
+                Text = @"Se le comunica que ha sido regsitrada una nueva actividad en el sistema de eventos del CERSEU"
+            };
+
+            using (var client = new MailKit.Net.Smtp.SmtpClient())
+            {
+                client.Connect("smtp.gmail.com", 587);
+                // Note: since we don't have an OAuth2 token, disable
+                // the XOAUTH2 authentication mechanism.
+                client.AuthenticationMechanisms.Remove("XOAUTH2");
+
+                // Note: only needed if the SMTP server requires authentication
+              
+                client.Authenticate("imanol123valera@gmail.com", "xxxxxxxx");
+                client.Send(message);
+                client.Disconnect(true);
+            }
+        }
+    }
+}
---
+++ sendmail/sendmail/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Mail;
+using System.Net.Mime;
+
+namespace sendmail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            GestorCorreo gestor = new GestorCorreo();
+
+            //Correo con archivos adjuntos
+            //MailMessage correo = new MailMessage("jaltez97@gmail.com",
+            //                                     "raul.armas@unmsm.edu.pe",
+            //                                     "Archivo de configuracíon",
+            //                                     "Por favor verificar adjunto.");
+
+            //string ruta = @"C:\Users\raula\source\repos\sendmail\sendmail\" + "Configuracion.xml";
+            //Attachment adjunto = new Attachment(ruta, MediaTypeNames.Application.Xml);
+            //correo.Attachments.Add(adjunto);
+            //gestor.EnviarCorreo(correo);
+
+            // Correo con HTML
+            gestor.EnviarCorreo("jaltez97@gmail.com",
+                                "Prueba1",
+                                "Mensaje en texto plano");
+
+            Console.WriteLine("Prueba de correo ok");
+            //// Correo de texto  
+            //gestor.EnviarCorreo("raul.armas@unmsm.edu.pe",
+            //                    "Prueba2",
+            //                    "<h1>Mensaje en HTML<h1><p>Contenido</p>",
+            //                    true);
+        }
+    }
+}
